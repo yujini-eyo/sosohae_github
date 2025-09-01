9249e43387cbe5244902f27bcd75c4af0d362e81
KOREAIT\yujinieyo <salt101050@gmail.com>
2025-09-01 11:46:24 +0900

    회원정보 db

---
+++ 이음.sql
@@ -0,0 +1,15 @@
+SELECT * FROM eumdb.member;
+
+INSERT INTO `eumdb`.`member`
+(`id`,
+`password`,
+`name`,
+`email`,
+`address`,
+`age`,
+`birth`,
+`phone`,
+`code`,
+`notes`)
+VALUES
+('yhy', '1234', '양희윤', 'yhy@naver.com', '대전 유성구', '25', '2001-11-11', '010-2345-6789', '3333', '장염');
